2fa9ed6e7f58cbdb1e8cb6f2fe9877ea33c0b871
Zishan Siddique <zishansiddique80@gmail.com>
2024-06-17 16:16:38 +0545

    setup.py added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="dummy",
+    version="0.0.01",
+    description="Just a dummy package",
+    author="zishan",
+    install_requires=["numpy", "pandas"],
+)
